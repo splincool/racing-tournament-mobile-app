c44fa5a121fe4c936e3e075095eac8d5e1b8d02b
splincool <mak.ivanov@gmail.com>
2019-12-17 23:38:26 +0300

    added champs refresh when we make pull gesture

--- screens/ScreenThree.js
+++ screens/ScreenThree.js
@@ -1,32 +1,47 @@
 import React from 'react';
-import { ActivityIndicator, Text, View, StyleSheet, Image, ScrollView  } from 'react-native';
+import { 
+  ActivityIndicator,
+  View, 
+  StyleSheet, 
+  ScrollView, 
+  FlatList,
+  SafeAreaView,
+  Button,
+  RefreshControl 
+} from 'react-native';
+import Constants from 'expo-constants';
 import Champ from '../components/Champ'
 export default class FetchExample extends React.Component {
 
   constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state ={ isLoading: true, refreshing: false}
   }
 
   componentDidMount(){
+    this.getData()
+  }
+
+  async getData() {
+    this.setState({refreshing: true})
     return fetch('https://us-central1-racing-tournament.cloudfunctions.net/getChampionships')
       .then((response) => response.json())
       .then((responseJson) => {
 
         this.setState({
           isLoading: false,
+          refreshing: false,
           dataSource: responseJson,
         }, function(){
 
         });
+        console.log('updated')
 
       })
       .catch((error) =>{
         console.error(error);
       });
   }
-
-
 
   render(){
 
@@ -39,22 +54,31 @@ export default class FetchExample extends React.Component {
     }
 
     return(
-      <ScrollView>
-        <View style={styles.container}>
-          <View>
-            {this.state.dataSource.map((item,i) => <Champ key={i} item={item}/>)}
+      <SafeAreaView style={styles.area}>
+        <ScrollView 
+          refreshControl={
+            <RefreshControl refreshing={this.state.refreshing} onRefresh={() => this.getData()} />
+          }
+        >
+          <View style={styles.container}>
+            <View>
+              {this.state.dataSource.map((item,i) => <Champ key={i} item={item}/>)}
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  area: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20
+    justifyContent: 'center'
   },
 });
