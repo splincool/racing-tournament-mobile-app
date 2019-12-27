3721dc3e2587f21b59cd8f4befaf36ccd882dd0e
Maksim Ivanov <mak.ivanov@gmail.com>
2019-12-27 22:32:30 +0300

    small refactoring

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {View,Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
--- screens/ChampDetailsScreen.js
+++ screens/ChampDetailsScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View, Text} from 'react-native'
 
 export const ChampDetailsScreen = ({ navigation }) => {
-  const champId = navigation.getParam('champId')
+  // const champId = navigation.getParam('champId')
 
   return (
     <View>
