452fba4ae321ee3ebe56c8208764304e6d1f0de9
Maksim Ivanov <mak.ivanov@gmail.com>
2020-01-03 21:44:17 +0300

    added segment control tabs

--- screens/ChampDetailsScreen.js
+++ screens/ChampDetailsScreen.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
+import SegmentedControlTab from "react-native-segmented-control-tab";
+
 
 export const ChampDetailsScreen = ({ navigation }) => {
   const champName = navigation.getParam('champName')
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  handleIndexChange = index => {
+    setSelectedIndex(index)
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Champ Details Screen: {champName}</Text>
+      <SegmentedControlTab
+        values={["Info", "Standings"]}
+        selectedIndex={selectedIndex}
+        onTabPress={handleIndexChange}
+      />
+      {selectedIndex === 0 && (
+        <View style={styles.body}>
+          <Text>Champ Details Screen: {champName}</Text>
+        </View>
+      )}
+      {selectedIndex === 1 && (
+        <Text> Tab two</Text>
+      )}
     </View>
   )
 }
@@ -15,6 +34,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 4,
+    paddingTop: 10
   },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
